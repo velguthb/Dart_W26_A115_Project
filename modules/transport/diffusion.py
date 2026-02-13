a67162e31e9138f4dadf0dfe944a69ae76c50404
Artemis Theodoridis <atheodoridis@ufl.edu>
2026-02-13 09:30:36 -0500

    Working (?) on transport
    A VERY simple 'working' version of the transport module which includes transport_step which can be called upon and goes through the different diffusion coefficients. Values are unphysical for now, but should be callable at the very least.

--- modules/transport/diffusion.py
+++ modules/transport/diffusion.py
@@ -1,159 +1,95 @@
-from fipy import Grid1D, CellVariable, FaceVariable, TransientTerm, DiffusionTerm
+from fipy import Grid1D, CellVariable
 import numpy as np
 
-from test_transport import get_diffusion_profile
+from modules.transport.transport import transport_step
+from modules.nuclear import nuc_burn
+from modules.eos import eos_functions
 
-# This is a stand in for your more robust diffusion function
-#def Dumnmy_get_diffusion(nx):
-   # D = np.ones(nx) *1e-2
-    #D[int(0.3*nx):int(0.5*nx)] += 1e-2 # Very approximate effect of a convective zone
-    #return D
+# -----------------------------
+# Grid setup
+# -----------------------------
 
-# Setting up grid / discritization. Most of this will be informed from inputs you recive from other modules
-m_min, m_max = 0, 1.0  
 nx = 100
+mesh = Grid1D(nx=nx, dx=1.0 / nx)
 
-# If you choose to use FiPy then you will need to work in its framework, this means you need to take the grid of 
-# masses george has generated and turn them into a Grid1D
+species = ["H1", "He4"]
 
-# There are alternate Grid1D constructors which let you take in a preexisting grid, try to look these up in the
-# FiPy documentation if you decide to go this route
-mesh = Grid1D(nx=nx, dx=(m_max - m_min) / nx)
+# FiPy variables for each species
+Y = {s: CellVariable(mesh=mesh, value=0.1) for s in species}
 
-# These are the initial conditions, I have done some very approximate stuff here to make an interesting
-# demo case. Generally you will recive these from either george or sasha
-species = ['H1', 'He4']
-Y = {s: CellVariable(mesh=mesh, value=0.1, hasOld=True) for s in species}
+# -----------------------------
+# Stellar structure
+# -----------------------------
 
-initial_H1 = np.where(mesh.cellCenters[0] < 0.5, 0.7, 0.2)
-Y['H1'].setValue(initial_H1)
-
-initial_He4 = np.where(mesh.cellCenters[0] < 0.5, 0.3, 0.8)
-Y['He4'].setValue(initial_He4)
-
-# I am using this python module FiPy, its a Finite Volume PDE solver
-# Finite volumes are good for this class of problem as they deal with conservation laws inherently
-# and tend to be more numerically stable than finite difference. 
-
-# These are variables which are different for each cell. Note how I am just using numpy arrays as the
-# values. You already calculate D so you might use that as the value
-r_cell = CellVariable(mesh=mesh, value=np.linspace(0.1, 1.0, nx))
-rho_cell = CellVariable(mesh=mesh, value=np.ones(nx))
-# ----------------------------------
-# Construct structure dict for transport
-# ----------------------------------
-
-m = mesh.cellCenters[0].value
-r = r_cell.value
-
-Hp = np.linspace(5e9, 5e10, nx)
-
-grad_rad = 0.4 * np.ones(nx)
-grad_ad  = 0.2 * np.ones(nx)
-grad_mu  = 0.01 * np.ones(nx)
-
-is_convective = np.zeros(nx, dtype=bool)
-is_convective[int(0.3*nx):int(0.5*nx)] = True
-
-alpha_MLT = 1.8
-l_mlt = alpha_MLT * Hp
-
-v_mlt = np.zeros(nx)
-superadiabatic = grad_rad - grad_ad
-v_mlt[is_convective] = 3e5 * np.sqrt(
-    np.maximum(superadiabatic[is_convective], 1e-4)
-)
-
+m = mesh.cellCenters[0].value  # "mass" coordinate
 structure = {
     "m": m,
-    "r": r,
-    "rho": rho_cell.value,
-    "Hp": Hp,
+    "rho": np.ones(nx),
+    "Hp": np.linspace(5e9, 5e10, nx),
     "K": np.ones(nx),
     "Cp": np.ones(nx),
-    "grad_rad": grad_rad,
-    "grad_ad": grad_ad,
-    "grad_mu": grad_mu,
-    "is_convective": is_convective,
-    "l_mlt": l_mlt,
-    "v_mlt": v_mlt,
+    "T": 1.5e7 * np.ones(nx),  # typical MS star core temp
+    "grad_rad": 0.4 * np.ones(nx),
+    "grad_ad": 0.2 * np.ones(nx),
+    "grad_mu": 0.01 * np.ones(nx),
+    "is_convective": np.zeros(nx, dtype=bool),
 }
 
-# ----------------------------------
-# Call YOUR diffusion module
-# ----------------------------------
+#convective region test
+structure["is_convective"][30:50] = True
 
-D_profile = get_diffusion_profile(structure)
+# Mixing length
+alpha_MLT = 1.8
+structure["l_mlt"] = alpha_MLT * structure["Hp"]
 
-D_cell = CellVariable(mesh=mesh, value=D_profile)
+# Convective velocities
+v = np.zeros(nx)
+v[structure["is_convective"]] = 1e2  # reduce for stability
+structure["v_mlt"] = v
 
-A_face = (4 * np.pi * r_cell.faceValue**2 * rho_cell.faceValue)**2 * D_cell.faceValue
+mu = 0.61  # mean molecular weight
+P = 1e17 * np.ones(nx)  # arbitrary pressure
+structure["rho"] = eos_functions.simple_eos(P, mu, structure["T"])
 
-eqs = {s: TransientTerm() == DiffusionTerm(coeff=A_face) for s in species}
+# -----------------------------
+# Convert FiPy → ndarray
+# -----------------------------
 
+X = np.vstack([Y[s].value for s in species]).T  # shape (nx, n_species)
 
+# -----------------------------
+# Time step
+# -----------------------------
+# small dt to satisfy CFL for diffusion
+dt = 1e-12
 
-# This is just some plotting code to make visualization
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+# Initial Sasha composition object
+comp_objects = None
 
-fig, ax = plt.subplots(figsize=(8, 5))
-ax.set_xlim(0, m_max)
-ax.set_ylim(0, 1.1)
-ax.set_xlabel('Mass Coordinate (m)')
-ax.set_ylabel('Molar Abundance (Yi)')
-ax.set_title('Species Abundance Evolution')
+# -----------------------------
+# Transport + Nuclear burn
+# -----------------------------
 
-lines = {}
-for name in species:
-    line, = ax.plot(mesh.cellCenters[0], Y[name].value, label=name, lw=2)
-    lines[name] = line
+X_new = transport_step(
+    X,
+    structure,
+    dt,
+    comp_objects=comp_objects,
+    f_ov=0.01,
+    alpha_sc=0.01,
+    alpha_th=1.0,
+    instantaneous_convection=True,
+)
 
-ax.legend()
+# -----------------------------
+# Putting back into FiPy
+# -----------------------------
 
-# Diffusion timescales are very different than nuclear timescales, therefore it is often
-# useful to solve these systems seperately (operator-splitting) using a much finer timestep
-dt = 1e-1
+for i, s in enumerate(species):
+    Y[s].setValue(X_new[:, i])
 
-# In this case I am letting matplotlib handle the timestepping loop, this is useful for animation
-# however, not great for actual code. You could replace this by just the inner for loop
-def update(frame):
-    for s in species: # Solve indeendencly for each species. This removes cross-diffusion
-        # Here you might do something like 
-        # new_abundances = nuclear(...)
-        # Y[s].setValue(new_abundances)
-
-        # Some complexity to think about here, though using
-        # a coroutine may make this a bit simpler to implement
-
-        Y[s].updateOld()
-        eqs[s].solve(var=Y[s], dt=dt)
-        
-        lines[s].set_ydata(Y[s].value)
-    
-    return list(lines.values())
-
-animation = FuncAnimation(fig, update, frames=2000, interval=50, blit=True)
-
-plt.show()
-
-# Some things to try
-#   1. Play around with getting your Diffision Function in there
-#   2. See how changing the timestep size affects results
-#   3. Try to get sashs outputs into here (they will come in as R_nuc) and then to return new abundances to george
-#   4. Check mass conservation, solvers have a tendency to let mass "leak" out, when this happens re-normalization stages are often required
-Collapse
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# -----------------------------
+# Print diagnostics
+# -----------------------------
+print("X_new shape:", X_new.shape)
+print("X_new sample values:", X_new[30:50])  # convective region
